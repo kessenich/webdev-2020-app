127ac18361e2f22e6dd3ee745a55e1ae00f97c02
Kevin Kessenich <kevin.kessenich@cofinpro.de>
2020-02-03 21:24:22 +0100

    fix title tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cicd-example-app'`, () => {
+  it(`should have as title 'WebDev 2020'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('cicd-example-app');
+    expect(app.title).toEqual('WebDev 2020');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cicd-example-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('WebDev 2020 app is running!');
   });
 });
